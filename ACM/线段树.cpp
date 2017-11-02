4380b06fa7e8ede84199c36e95ad2ae801efa5a4
mutinner <mutinner@163.com>
2017-11-02 16:50:01 +0800

    线段树更正

--- ACM/线段树.cpp
+++ ACM/线段树.cpp
@@ -8,7 +8,7 @@ const ll inf=1<<30;
 
 struct sgetree{
 	int L,R;
-	ll sum,maxn,minn;	
+	ll sum,maxn,minn;
 };
 
 vector<sgetree> cnt;
@@ -16,6 +16,7 @@ vector<ll> add;
 
 void build(int x,int L,int R){
 	cnt[x]=sgetree{L,R,0,-inf,inf};
+ 	add[x]=0;
 	if(L==R)	return ;
 	int mid=(L+R)>>1;
 	build(x*2,L,mid);
@@ -32,9 +33,7 @@ void insert(int x,int pos,ll num){
 	int mid=(cnt[x].L+cnt[x].R)>>1;
 	if(pos<=mid)	insert(x*2,pos,num);
 	else	insert(x*2+1,pos,num);
-	cnt[x].sum=cnt[x*2].sum+cnt[x*2+1].sum;
-	cnt[x].maxn=max(cnt[x*2].maxn,cnt[x*2+1].maxn);
-	cnt[x].minn=min(cnt[x*2].minn,cnt[x*2+1].minn);
+	pushup(x);
 }
 
 ll querysum(int x,int L,int R){
@@ -57,12 +56,12 @@ void pushup(int x){
 	cnt[x].sum=cnt[x*2].sum+cnt[x*2+1].sum;
 }
 
-void pushdown(int x,int L,int R){
+void pushdown(int x){
 	if(add[x]!=0){
 		add[x*2]+=add[x];   add[x*2+1]+=add[x];
-		int mid=(L+R)>>1;
-		cnt[x*2].sum+=add[x]*(mid-L+1);
-		cnt[x*2+1].sum+=add[x]*(R-mid);
+		int mid=(cnt[x].L+cnt[x].R)>>1;
+		cnt[x*2].sum+=add[x]*(mid-cnt[x].L+1);
+		cnt[x*2+1].sum+=add[x]*(cnt[x].R-mid);
 		add[x]=0;
 	}
 }
@@ -74,17 +73,23 @@ void update(int x,int L,int R,ll addv){
 		return ;
 	}
 	pushdown(x,L,R);
-	int mid=(L+R)>>1;
-	if(L<=mid)    update(x*2,L,mid,addv);
-	else    update(x*2+1,mid+1,R,addv);
+	int mid=(cnt[x].L+cnt[x].R)>>1;
+	if(R<=mid)  update(x*2,L,R,addv);
+	else if(L>mid)  update(x*2+1,L,R,addv);
+	else {
+		update(x*2,L,mid,addv);
+		update(x*2+1,mid+1,R,addv);
+	}
 	pushup(x);
 }
 
 ll query(int x,int L,int R){
 	if(L==cnt[x].L&&R==cnt[x].R)    return cnt[x].sum;
 	pushdown(x,L,R);
-	int mid=(L+R)>>1;
-	return query(x*2,L,mid)+query(x*2+1,mid+1,R);
+	int mid=(cnt[x].L+cnt[x].R)>>1;
+	if(R<=mid)  return query(x*2,L,R);
+	else if(L>mid)  return query(x*2+1,L,R);
+	else    return query(x*2,L,mid)+query(x*2+1,mid+1,R);
 }
 
 int main(){
