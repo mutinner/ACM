a7909a5af7ae958f4cb5f51ae6b82cacda3023be
mutinner <mutinner@163.com>
2017-11-05 15:56:58 +0800

    线段树更正

--- ACM/线段树.cpp
+++ ACM/线段树.cpp
@@ -15,15 +15,15 @@ vector<sgetree> cnt;
 vector<ll> add;
 
 void pushup(int x){
-	cnt[x].sum=cnt[x*2].sum+cnt[x*2+1].sum;
+	cnt[x].sum=cnt[x<<1].sum+cnt[x<<1|1].sum;
 }
 
 void pushdown(int x){
 	if(add[x]!=0){
-		add[x*2]+=add[x];   add[x*2+1]+=add[x];
+		add[x<<1]+=add[x];   add[x<<1|1]+=add[x];
 		int mid=(cnt[x].L+cnt[x].R)>>1;
-		cnt[x*2].sum+=add[x]*(mid-cnt[x].L+1);
-		cnt[x*2+1].sum+=add[x]*(cnt[x].R-mid);
+		cnt[x<<1].sum+=add[x]*(mid-cnt[x].L+1);
+		cnt[x<<1|1].sum+=add[x]*(cnt[x].R-mid);
 		add[x]=0;
 	}
 }
@@ -33,8 +33,8 @@ void build(int x,int L,int R){
 	add[x]=0;
 	if(L==R)	return ;
 	int mid=(L+R)>>1;
-	build(x*2,L,mid);
-	build(x*2+1,mid+1,R);
+	build(x<<1,L,mid);
+	build(x<<1|1,mid+1,R);
 	pushup(x);
 }
 
@@ -46,17 +46,17 @@ void insert(int x,int pos,ll num){
 		return ;
 	}
 	int mid=(cnt[x].L+cnt[x].R)>>1;
-	if(pos<=mid)	insert(x*2,pos,num);
-	else	insert(x*2+1,pos,num);
+	if(pos<=mid)	insert(x<<1,pos,num);
+	else	insert(x<<1|1,pos,num);
 	pushup(x);
 }
 
 ll querysum(int x,int L,int R){
 	if(L==cnt[x].L&&R==cnt[x].R)	return cnt[x].sum;
 	int mid=(cnt[x].L+cnt[x].R)>>1;
-	if(R<=mid)	return querysum(x*2,L,R);
-	else if(L>mid) return	querysum(x*2+1,L,R);
-	else	return querysum(x*2,L,mid)+querysum(x*2+1,mid+1,R);
+	if(R<=mid)	return querysum(x<<1,L,R);
+	else if(L>mid) return	querysum(x<<1|1,L,R);
+	else	return querysum(x<<1,L,mid)+querysum(x<<1|1,mid+1,R);
 }
 
 void update(int x,int L,int R,ll addv){
@@ -67,11 +67,11 @@ void update(int x,int L,int R,ll addv){
 	}
 	pushdown(x);
 	int mid=(cnt[x].L+cnt[x].R)>>1;
-	if(R<=mid)  update(x*2,L,R,addv);
-	else if(L>mid)  update(x*2+1,L,R,addv);
+	if(R<=mid)  update(x<<1,L,R,addv);
+	else if(L>mid)  update(x<<1|1,L,R,addv);
 	else {
-		update(x*2,L,mid,addv);
-		update(x*2+1,mid+1,R,addv);
+		update(x<<1,L,mid,addv);
+		update(x<1|1,mid+1,R,addv);
 	}
 	pushup(x);
 }
@@ -80,9 +80,9 @@ ll query(int x,int L,int R){
 	if(L==cnt[x].L&&R==cnt[x].R)    return cnt[x].sum;
 	pushdown(x);
 	int mid=(cnt[x].L+cnt[x].R)>>1;
-	if(R<=mid)  return query(x*2,L,R);
-	else if(L>mid)  return query(x*2+1,L,R);
-	else    return query(x*2,L,mid)+query(x*2+1,mid+1,R);
+	if(R<=mid)  return query(x<<1,L,R);
+	else if(L>mid)  return query(x<<1|1,L,R);
+	else    return query(x<<1,L,mid)+query(x<<1|1,mid+1,R);
 }
 
 int main(){
