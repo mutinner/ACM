3103e3395e4ff7d047e5e9c6aa46d21eea5de58d
mutinner <mutinner@163.com>
2017-12-01 21:55:15 +0800

    2017/12/1

--- ACM/tarjan.cpp
+++ ACM/tarjan.cpp
@@ -3,25 +3,47 @@
 #include<vector>
 using namespace std;
 
-vector<int> G[maxn];
-int dfn[maxn],low[maxn],stack[maxn],father[maxn];
-bool vis[maxn];
+int head[maxn];
+int dfn[maxn],low[maxn],color[maxn];
+int stack[maxn],father[maxn],vis[maxn];
 int cnt,top,con;
 vector<int> cut;
 
-void tarjan(int u,int pre){
+struct e{
+	int v,used,next;
+}edge[maxn*4];
+
+void init(int n){
+	for(int i=0;i<n+10;i++){
+		head[i]=-1;
+		vis[i]=dfn[i]=low[i]=0;
+	}
+	cnt=cur=top=0;
+}
+
+void add(int u,int v){
+	edge[cur].v=v;
+	edge[cur].used=0;
+	edge[cur].next=head[u];
+	head[u]=cur++;
+}
+
+void tarjan(int u){
 	father[u]=pre;
 	dfn[u]=low[u]=++cnt;
-	vis[u]=true;
+	vis[u]=1;
 	stack[++top]=u;
-	for(int i=0;i<G[u].size();i++){
-		int v=G[u][i];
-		if(v==pre)	continue;
-		if(dfn[v]==0){
-			tarjan(v,u);
-			low[u]=min(low[u],low[v]);
+	for(int i=head[u];i!=-1;i=edge[i].next){
+		if(edge[i].used==0){
+			edge[i].used=edge[i^1].used=1;
+			int v=edge[i].v;
+			if(dfn[v]==0){
+				father[v]=u;
+				tarjan(v);
+				low[u]=min(low[u],low[v]);
+			}
+			else if(vis[v]==1)	low[u]=min(low[u],dfn[v]);
 		}
-		else if(vis[v])	low[u]=min(low[u],dfn[v]);
 	}
 	if(dfn[u]==low[u]){
 		con++;
