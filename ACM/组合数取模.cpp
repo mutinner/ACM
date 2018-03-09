61e2bf5d438743d383508f3d034e0a63afac8833
mutinner <mutinner@163.com>
2018-03-09 21:35:31 +0800

    组合数

--- ACM/组合数取模.cpp
+++ ACM/组合数取模.cpp
@@ -2,43 +2,47 @@
 using namespace std;
 
 typedef long long ll;
-const int N=2005;
-const ll p=1e9+7;
-long long fac[N];
+const ll mod=1e9+7;
+const int maxn 2e5=10;
 
-void init(long long p){
-	fac[0]=1;
-	for(long long i=1;i<=p;i++){
-		fac[i]=fac[i-1]*i%p;
+ll fac[maxn],finv[maxn],inv[maxn];
+
+void init(){
+	inv[1]=1;
+	for(int i=2;i<maxn;i++)	inv[i]=(mod-mod/i)*inv[mod%i]%mod;
+	fac[0]=finv[0]=1;
+	for(int i=1;i<maxn;i++){
+		fac[i]=fac[i-1]*i%mod;
+		finv[i]=finv[i-1]*inv[i]%mod;
 	}
 }
 
-long long exp_mod(long long a,long long b,long long p){
-	long long tmp=a%p,ans=1;
+ll comb(int n,int m){
+	if(m<0||m>n)	return 0;
+	return fac[n]*finv[n-m]%mod*finv[m]%mod;
+}
+
+#include<iostream>
+using namespace std;
+
+ll fac[maxn];
+
+void init(){
+	fac[0]=1;
+	for(int i=1;i<maxn;i++)	fac[i]=fac[i-1]*i%mod;
+}
+
+ll pow(ll a,ll b){
+	ll ans=1;	a%=mod;
 	while(b){
-		if(b&1){
-			ans=ans*tmp%p;
-		}
-		tmp=tmp*tmp%p;
+		if(b&1)	ans=ans*a%mod;
+		a=a*a%mod;
 		b>>=1;
 	}
 	return ans;
 }
 
-long long comb(long long n,long long m){
-	if(m>n){
-		return 0;
-	}
-	return fac[n]*exp_mod(fac[m]*fac[n-m],p-2)%p;
-}
-
-long long Lucas(long long n,long long m){
-	if(m==0)	return 1;
-	return (comb(n%p,m%p)*Lucas(n/p,m/p)%p);
-}
-
-int main(){
-	init(p);
-	cout<<Lucas(n,m,p)<<endl;
-	return 0;
+ll comb(ll n,llm){
+	if(m>n)	return 0;
+	return fac[n]*pow(fac[m]*fac[n-m],mod-2)%mod;
 }
